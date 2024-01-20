f72f2122c69b5045e3f1c7eee82c6a663979872b
Josep2410 <josephpinoy2@yahoo.com>
2024-01-20 02:22:47 -0600

    getTotal Price in DisplayTotal Component

--- src/components/DisplayTotal.jsx
+++ src/components/DisplayTotal.jsx
@@ -1,12 +1,17 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import Context from '../Context/MyContext'
 
-export default function DisplayTotal() {
-  const {itemsInCart , totalCartItems,clearCart} = useContext(Context)
+export default function DisplayTotal({setTotal = () => {} }) {
+  const {itemsInCart , totalCartItems} = useContext(Context)
   const subTotal = (Math.round((itemsInCart.reduce((total , curr) => total + (curr.numberInCart * curr.price) , 0)) * 100) /100)
-  const tax =  (Math.round((subTotal * .0825) * 100) / 100).toFixed(2)
-  const total = (parseInt(tax)+ subTotal)
- 
+  const tax =  (Math.round((subTotal * .0825) * 100) / 100)
+  //const total = ((Math.round((tax + subTotal) * 100)) / 100)
+  const total = (tax + subTotal).toFixed(2)
+
+  useEffect(() => {
+    setTotal(total)
+  }, [])
+
   return(
     <section className="totalPage">
       <p>
