d075c86b91caa161f9d4107a93f0c03e15bf1d2a
Josep2410 <josephpinoy2@yahoo.com>
2024-01-23 15:39:59 -0600

    Stylistic changes to Home Component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,23 +10,27 @@ export default function Home() {
     <div className="homePage">
     
       <Title/> 
-      <ImagePane img="/images/pupusasDish.jpg" text="Authentic Salvadoran food" alt="pupusas dish" link=""/>
-      <h4>Come and dine with us</h4>
-      <Separator />
-      <h3>New Here?</h3>
-      <h4>Checkout our most popular items</h4>
-      <ImagePane img="images/gallinaIndia.jpg" text="Browse our most popular Items" alt="gallina india" link="/popular"/>
-      <Separator />
-      <h3>Don't have an account?</h3>
-      <h3>Create One</h3>
-      <ImagePane img="/images/Salvadoran-Quesadilla.jpg.webp" text="Create Account" link="/createAccount"/>
-      <h4>Earn reward points, view your previous orders and much more
-        <span>Already have one ? <Link to="/login">Log in</Link></span>
-      </h4>
+     <main>
+     <ImagePane img="/images/pupusasDish.jpg" text="Authentic Salvadoran food" alt="pupusas dish" link=""/>
+        <h4>Come and dine with us</h4>
+        <Separator />
+        <h3>New Here?</h3>
+        <h4>Checkout our most popular items</h4>
+        <ImagePane img="images/gallinaIndia.jpg" text="Browse our most popular Items" alt="gallina india" link="/popular"/>
+        <Separator />
+        <h3>Don't have an account? <span>Create One</span></h3>
       
-      <Separator />
+        <ImagePane img="/images/Salvadoran-Quesadilla.jpg.webp" text="Create Account" link="/createAccount"/>
+        <h4>Earn reward points, view your previous orders and much more
+          <span>Already have one ? <Link to="/login">Log in</Link></span>
+        </h4>
+        
+        <Separator />
+        
+        <ImagePane img="/images/ole-spanish-restaurant.jpg" text="Established in 1965" link=""/>
+     </main>
       
-      <ImagePane img="/images/ole-spanish-restaurant.jpg" text="Established in 1965" link=""/>
+    
     </div>
   )
 }
