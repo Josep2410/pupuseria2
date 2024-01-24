fc552cb8848d5662d1fccae43ac4605db7853921
Josep2410 <josephpinoy2@yahoo.com>
2024-01-24 17:03:06 -0600

    Used a third party pckg Modal

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,9 +1,15 @@
-import React, {useContext} from 'react'
+import React, {useState, useContext} from 'react'
 import {useNavigate , Link} from 'react-router-dom'
 import Title from '../components/Title'
 import Context from '../Context/MyContext'
+import PureModal from 'react-pure-modal';
+import 'react-pure-modal/dist/react-pure-modal.min.css';
+
 
 export default function Profile() {
+
+  const [modal, setModal] = useState(false);
+
   const { usersURL , currentUser , setCurrentUser , users , setUsers} = useContext(Context)
   const navigate = useNavigate()
   const logOut = () => {
@@ -31,8 +37,28 @@ const deleteAccount = async () => {
     <section className="profilePage">
       <Title />
       <div><Link to="previousOrders"><button>Previous Orders</button></Link></div>
-      <div>   <button onClick={logOut}>Logout </button></div>
-      <div><button onClick={deleteAccount} style={{color : '#BC0000'}}>Delete Account</button></div>
+      <div><button onClick={logOut}>Logout </button></div>
+      <div><button onClick={() => setModal(true)} style={{color : '#BC0000'}}>Delete Account</button></div>
+      <PureModal
+        header="Are you sure? "
+        footer={
+          <div>
+            <button onClick={() => setModal(false)}>Cancel</button>
+            <button onClick={deleteAccount} className='danger'>YES, I'm sure</button>
+          </div>
+        }
+        isOpen={modal}
+        draggable={true}
+        closeButton="X"
+        closeButtonPosition="header"
+        onClose={() => {
+        setModal(false);
+        return true;
+    }}
+       
+  >
+    <p>Deleting your account will delete all your information</p>
+  </PureModal>
     </section>
   )
 }
