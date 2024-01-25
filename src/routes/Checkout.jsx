21e2e0bb242f38104179bfa768fdb1a1e5d949cf
Josep2410 <josephpinoy2@yahoo.com>
2024-01-25 15:45:56 -0600

    fixed currentUser null bug

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -1,10 +1,12 @@
-import React , {useState ,useContext} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React , {useState ,useContext, useEffect } from 'react'
+import { useNavigate} from 'react-router-dom'
 import DisplayTotal from '../components/DisplayTotal'
 import Title from '../components/Title'
 import Context from '../Context/MyContext'
+import DisplayNoContent from '../components/DisplayNoContent'
 import emailjs from 'emailjs-com'
 import {format} from 'date-fns'
+
 
 export default function Checkout() {
   const navigate = useNavigate()
@@ -12,38 +14,54 @@ export default function Checkout() {
 
   const [total , setTotal] = useState(0)
   const [date , setDate] = useState(format( new Date() , 'MM/dd/yyyy HH:mm:ss'))
+  const [isLoading , setIsLoading] = useState(true)
   const confirmationNum = makeid(8)
  
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
   const orderDetails = itemsInCart.map(item => `\tItem : ${item.item} \t Qty : ${item.numberInCart} \t Price : $${item.numberInCart * item.price}`) 
   const emailBody = 
     `Order placed on ${date}\nTotal of $${total} was charged to your account.\nOrder details : \n${orderDetails.join('\n')}`
 
-
- 
-
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
+    
     e.preventDefault()
-    updateUser('previousOrders', { date, total, itemsInCart})
     emailjs.sendForm('service_nerl3hs', 'contact_form', e.target, 'C865dYJwizaxcQPPT')
-      .then(()=> console.log('Success'), (err) => console.log(err))
+      .then(()=> console.log('Successfully Submitted Email'), (err) => console.log(err))
+    updateUser('previousOrders', { date, total, itemsInCart})
     clearCart()
     navigate('/home')
   }
 
+  console.log(itemsInCart)
+  if(!itemsInCart.length){
+   return <DisplayNoContent message="No items in Cart" linkTo="/home" linkContent="Return to Home"/>
+  }
+
   return (
    <> 
-    <Title />
-    <div className="checkoutDiv">
-      <DisplayTotal setTotal={setTotal}/>
-      <form id="submitOrderForm" onSubmit={handleSubmit}>
-        <input type="hidden" value={currentUser.email} name="recipient" />
-        <input type="hidden" value={currentUser.name} name="to_name"/>
-        <input type="hidden" value={emailBody} name="message" />
-        <input type="hidden" value={confirmationNum} name="confirmationNumber" />
-        <input type="hidden" value="MI PUPUSERIA" name="from_name" />
-        <button type='submit'>Submit order</button>
-      </form>
-    </div>
+   {isLoading? (
+     <h1>Loading...</h1>
+       
+   ) : (
+    <>
+        <Title />
+       <div className="checkoutDiv">
+       <DisplayTotal setTotal={setTotal} />
+         <form id="submitOrderForm" onSubmit={handleSubmit} >
+           <input type="hidden" value={currentUser.name} name="to_name"/>
+           <input type="hidden" value={currentUser.email} name="recipient" />
+           <input type="hidden" value={emailBody} name="message" />
+           <input type="hidden" value={confirmationNum} name="confirmationNumber" />
+           <input type="hidden" value="MI PUPUSERIA" name="from_name" />
+           <button type='submit'>Submit order</button>
+         </form>
+       </div>
+       </>
+   )
+  }
    </>
   )
 }
